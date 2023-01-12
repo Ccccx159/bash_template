319da2442bed5831f7efda750db1fdd8c38ba6d4
xu4nch3n <ccccx1594@gmail.com>
2023-01-12 14:41:57 +0800

    新增文件相关操作函数

--- bash_template.sh
+++ bash_template.sh
@@ -32,7 +32,7 @@ function log () {
   printf "%s [%-5s]   %s\033[0m\n" "${TIME}" "$1" "$2"
 }
 
-function clean () {
+function fn_Clean () {
     log Warn "this operation will clean all the temporary files"
     read -r -p "Are You Sure To Begin Cleanning? [Y/n] " input
     case $input in
@@ -54,7 +54,7 @@ function clean () {
     esac
 }
 
-function parseArgs () {
+function fn_ParseArgs () {
   while getopts "a:e:f:hc" opts; do
     case $opts in
       e)
@@ -62,7 +62,7 @@ function parseArgs () {
         ;;
       a)
         array_str=$OPTARG
-        echo $array_str
+        # echo $array_str
         array_info=(${array_str//,/ })
         ;;
       f)
@@ -73,7 +73,7 @@ function parseArgs () {
         exit 0
         ;;
       c)
-        clean
+        fn_Clean
         exit 0
         ;;
       \?)
@@ -83,7 +83,7 @@ function parseArgs () {
         ;;
       :)
         log Error "Option [-$OPTARG] requires an argument"
-        usage
+        usage 
         exit 1
         ;;
     esac
@@ -95,8 +95,105 @@ function parseArgs () {
   fi
 }
 
-parseArgs "$@"
+# 获取特定路径下，指定后缀的文件路径
+function fn_GetFileList() {
+  local suffix=$1
+  local dir=$2
+  local fileList=$(ls ${dir}/*.${suffix})
+  echo ${fileList}
+
+  return $?
+}
+
+# 查找指定文件是否存在 file 支持正则表达式
+function fn_QueryFileIsExist () {
+  local file=$1
+  local dir=$2
+  local fileNum=$(ls ${dir}/${file} 2> /dev/null | wc -l)
+  if [[ "0" == "${fileNum}" ]]; then
+    log Error "${dir}/${file} : No such file or directory"
+  else
+    log Info "${dir}/${file} exist"
+    for file_opt in $(ls ${dir}/${file})
+    do
+      log Debug "${file_opt}"
+    done
+  fi
+  return ${fileNum}
+}
+
+# #： 表示从左边算起第一个
+# %： 表示从右边算起第一个
+# ##：表示从左边算起最后一个
+# %%：表示从右边算起最后一个
+# *： 表示要删除的内容
+
+# 也可以使用 basename, echo $(basename ${file})
+# 已知后缀时，basename可以直接去除后缀 echo $(basename ${file} ${suffix})
+# 截取文件名
+function fn_GetFileName () {
+  local file=$1
+  local name=${file##*/}
+  echo ${name}
+  return $?
+}
+
+# 截取不带后缀的文件名
+function fn_GetFileNameWithoutSuffix () {
+  local file=$1
+  local fullname=${file##*/}
+  local name=${fullname%%.*}
+  echo ${name}
+  return $?
+}
+
+# 截取文件路径
+function fn_GetFilePath () {
+  local file=$1
+  local path=${file%/*}
+  echo ${path}
+  return $?
+}
+
+function fn_GetFileSuffix () {
+  local file=$1
+  local fileName=$(fn_GetFileName ${file})
+  local suffix=${fileName#*.}
+  echo ${suffix}
+  return $?
+}
+
+fn_ParseArgs "$@"
 log Info "root dir: ${root_dir}"
 log Debug "exec_name: ${exec_name}"
 log Warn "workflow: ${workflow}"
 log Error "array_info: ${array_info[*]}"
+
+echo ""
+log Warn "function fn_GetFileList test begin..."
+files=$(fn_GetFileList "jpg" "${root_dir}/.tmp/GetFileList/")
+for file_opt in ${files[*]}
+do
+  log Debug "${file_opt}"
+done
+log Warn "function fn_GetFileList test finish..."
+
+echo ""
+log Warn "function fn_QueryFileIsExist test begin..."
+fn_QueryFileIsExist "*.sh" "."
+if [[ 0 -ne $? ]]; then
+  log Info "Query Successfully"
+else
+  log Warn "Query finished, but No such file or directory...Please check, if it's necessary"
+fi
+log Warn "function fn_QueryFileIsExist test finish..."
+
+echo ""
+log Warn "function fn_GetFilexxx test begin..."
+filePath="$(pwd)/$0"
+log Debug "file [${filePath}]"
+log Debug "GetFileName: $(fn_GetFileName ${filePath})"
+log Debug "GetFileNameWithoutSuffix: $(fn_GetFileNameWithoutSuffix ${filePath})"
+log Debug "GetFilePath: $(fn_GetFilePath ${filePath})"
+log Debug "GetFileSuffix: $(fn_GetFileSuffix ${filePath})"
+log Warn "function fn_GetFilexxx test finish..."
